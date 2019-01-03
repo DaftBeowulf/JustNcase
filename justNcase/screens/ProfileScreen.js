a3c2ec003d2c2875ddc0bd9f10a6608f0df8ac22
Chris Roth <c.roth.lambda@gmail.com>
2019-01-03 13:27:41 -0600

    updates to styling in profile page added PNG icons folder pushing to merge with Noah.

--- justNcase/screens/ProfileScreen.js
+++ justNcase/screens/ProfileScreen.js
@@ -24,7 +24,7 @@ export default class ProfileScreen extends React.Component{
             this.setState({
                 user: response.data
             })
-            .catch(err=>{
+            .catch(err=>{I 
                 console.log
             })
         })
@@ -32,19 +32,26 @@ export default class ProfileScreen extends React.Component{
 
     render(){
         return (
-            <View style={styles.container}>
-            <View style={{backgroundColor: '#1EB865', width: '100%',}}>
-                <Text style={styles.userText}>{this.state.user.username}</Text>
-            </View>
-            <View syle={{flex: 1, flexDirection:'row'}}>
-                <Text>
-                    Emergency Contacts
-                </Text>
-                <Text>
-                    Manage List
-                </Text>
-            </View>
-            </View>
+            <ScrollView style={styles.container}>
+                <View>
+                    <View style={{backgroundColor: '#1EB865', height:160, justifyContent:'space-around', flexDirection:'row',}}>
+                        <View >
+                            <Image source={require('../assets/png_icons/user.png')} style={{height:40, width:40, margin:8, }}/>
+                        </View>
+                        <View>
+                            <Text style={styles.userText}>{this.state.user.username}</Text>
+                            <Text style={styles.userText}>{this.state.user.email}</Text>
+                            <Text style={styles.userText}>{this.state.user.phone_number}</Text>
+                        </View>
+                     </View>
+            
+                     <View style={styles.smallText}>
+                       <Text>Emergency Contacts  </Text> 
+                       <Text>Manage List</Text>
+                    </View>
+
+                </View>
+            </ScrollView>
            
         )
     }
@@ -52,12 +59,21 @@ export default class ProfileScreen extends React.Component{
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#dcffcc',
-        alignItems: 'center',
         flex:1,
+        backgroundColor: '#e9fce0',
     },
     userText:{
+        alignSelf: 'center',
         fontSize: 22,
+        color:'white',
+        margin:2,
 
-    }
+    },
+    smallText:{
+        flex: 3,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin:6,
+        fontSize:12
+    },
 })
