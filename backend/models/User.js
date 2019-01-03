39b48f165f268619f71ece8608c65b1f0feb76a6
DaftBeowulf <ajmboosh@gmail.com>
2019-01-02 22:37:35 -0600

    refactored GET user by id to GET user by username. still tinkering with timer
    Co-authored-by: Mark-Hermansen <markwilliams.aus@gmail.com>

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const UserSchema = new Schema ({
+const UserSchema = new Schema({
   username: String,
-  contacts: [{ name: String, number: Number}],
-  events: [{ name: String, eventDuration: Number, checkinInterval: Number }]
-})
+  contacts: [{ name: String, number: Number }],
+  events: [
+    {
+      name: String,
+      eventDuration: Number,
+      checkinInterval: Number,
+      checkedIn: { type: Boolean, default: false }
+    }
+  ]
+});
 
-const Users = mongoose.model('User', UserSchema)
+const Users = mongoose.model("User", UserSchema);
 
-module.exports = Users
+module.exports = Users;
