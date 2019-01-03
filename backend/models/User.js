8ada2e1f54939bd9abc36b0c2a588bf9b99ccff1
DaftBeowulf <ajmboosh@gmail.com>
2019-01-03 01:06:16 -0600

    asynchronous calls to db allowed for checkins after user has checked in
    Co-authored-by: Mark-Hermansen <markwilliams.aus@gmail.com>

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,14 +5,12 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: String,
   contacts: [{ name: String, number: Number }],
-  events: [
-    {
-      name: String,
-      eventDuration: Number,
-      checkinInterval: Number,
-      checkedIn: { type: Boolean, default: false }
-    }
-  ]
+  events: {
+    name: String,
+    eventDuration: Number,
+    checkinInterval: Number,
+    checkedIn: { type: Boolean, default: false }
+  }
 });
 
 const Users = mongoose.model("User", UserSchema);
