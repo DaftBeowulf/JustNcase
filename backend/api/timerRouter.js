5120156ceac0977a1146b473cd681a38e706a7d4
DaftBeowulf <ajmboosh@gmail.com>
2019-01-03 17:28:29 -0600

    added ability to pull lat/long data from db and create dynamic url for googlemaps static map image, commented where demo code differes from future production

--- backend/api/timerRouter.js
+++ backend/api/timerRouter.js
@@ -7,14 +7,29 @@ const client = require("twilio")(accountSid, authToken);
 
 const Users = require("../models/User");
 
-function intervalFunc() {
+// dynamically creates image url for map with latest geodata latitudes and longitudes off the db.
+// for demo purposes, this function will not be run inside fireSMS and the example url is hard-coded in the .env file
+function retrieveUrl(event) {
+  const colors = ["blue", "green", "yellow", "red"];
+  let gmapUrl =
+    "https://maps.googleapis.com/maps/api/staticmap?&size=300x300&scale=2&maptype=hybrid&";
+  event.geoData.forEach((point, index) => {
+    gmapUrl += `markers=color:${colors[index]}%7C${point}&`;
+  });
+  gmapUrl += `key=${process.env.GOOGLE_MAP_API_KEY}`;
+  return gmapUrl;
+}
+
+function fireSMS(event) {
   if (accountSid === "not authorized" || authToken === "not authorized") {
     console.log("SMS fire--twilio not authorized");
   } else {
     console.log("SMS fire");
+
     client.messages
       .create({
-        body: "Adam missed his check in",
+        // body: retrieveUrl(event), // in production, this function would fire and return the dynamic url
+        body: process.env.GEO_IMAGE_URL, // in development/demo, hardcoding the url for the image link in
         from: twilioPhone,
         to: contactPhone
       })
@@ -81,7 +96,8 @@ router.get("/start/:_id", async (req, res) => {
       }
     } else {
       console.log(count);
-      intervalFunc();
+      // passing event in to take geodata off most recent db call to build googlemaps URL for SMS fire
+      fireSMS(event);
     }
   }, interval);
 });
