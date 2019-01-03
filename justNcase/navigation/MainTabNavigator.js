6302bb0961d749c8148f12d324fa3c50ddfa85dc
Noah Amar <ne.amar@outlook.com>
2019-01-02 19:00:36 -0600

    main tab navigator edits

--- justNcase/navigation/MainTabNavigator.js
+++ justNcase/navigation/MainTabNavigator.js
@@ -3,52 +3,15 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
-import Home from '../screens/Home';
+import LoginScreen from '../screens/LoginScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+const LoginStack = createStackNavigator({
+  Login: LoginScreen,
 });
-
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
-const Test = createStackNavigator({
-  Home: HomeScreen,
-});
-
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Test',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
-const HomePage = createStackNavigator({
-  Home: Home,
-});
-Home.navigationOptions = {
-  tabBarLabel: 'HomePage',
+LoginStack.navigationOptions = {
+  tabBarLabel: 'Login',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
@@ -90,9 +53,7 @@ SettingsStack.navigationOptions = {
 };
 
 export default createBottomTabNavigator({
-  HomeStack,
-  Home,
+  LoginStack,
   LinksStack,
   SettingsStack,
-  Test,
 });
