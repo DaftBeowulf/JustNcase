de8edf9bcfd20e3b58cca20427df9eb0dde488c3
DaftBeowulf <ajmboosh@gmail.com>
2019-01-03 20:45:24 -0600

    implemented fireUserSMS function so user gets a text each time they reach a checkin, after a calculated interval has passed, a timeOut runs a check to see if user has checked in. if yes, changed checkedIn back to false, of no, send SMS with map data to contact's number (for demo purposes, contactSMS and userSMS are same)

--- backend/api/timerRouter.js
+++ backend/api/timerRouter.js
@@ -9,27 +9,48 @@ const Users = require("../models/User");
 
 // dynamically creates image url for map with latest geodata latitudes and longitudes off the db.
 // for demo purposes, this function will not be run inside fireSMS and the example url is hard-coded in the .env file
-function retrieveUrl(event) {
+function retrieveUrl(user) {
   const colors = ["blue", "green", "yellow", "red"];
   let gmapUrl =
     "https://maps.googleapis.com/maps/api/staticmap?&size=300x300&scale=2&maptype=hybrid&";
-  event.geoData.forEach((point, index) => {
+  user.events.geoData.forEach((point, index) => {
     gmapUrl += `markers=color:${colors[index]}%7C${point}&`;
   });
   gmapUrl += `key=${process.env.GOOGLE_MAP_API_KEY}`;
   return gmapUrl;
 }
 
-function fireSMS(event) {
+function fireContactSMS(userCheck) {
   if (accountSid === "not authorized" || authToken === "not authorized") {
-    console.log("SMS fire--twilio not authorized");
+    console.log("contactSMS fire attempt--twilio not authorized");
   } else {
-    console.log("SMS fire");
-
     client.messages
       .create({
-        // body: retrieveUrl(event), // in production, this function would fire and return the dynamic url
-        body: process.env.GEO_IMAGE_URL, // in development/demo, hardcoding the url for the image link in
+        // body: retrieveUrl(user), // in production, this function would fire and return the dynamic url
+        body: `You are receiving this message because ${
+          userCheck.username
+        } has missed a scheduled checkin with JustNcase. Here is a link to their last know locations: ${
+          process.env.GEO_IMAGE_URL
+        }`, // in development/demo, hardcoding the url for the image link in
+        from: twilioPhone,
+        to: contactPhone
+      })
+      .then(message => {
+        console.log(message.sid);
+      })
+      .done();
+  }
+}
+
+// in production, the number this fires to will be separate from that used in fireContactSMS. for demo/dev purposes, both will be sent to same number
+// fires an SMS to the user's number alerting them before each checkin deadline that the checkin is approaching and they can now check in
+function fireUserSMS(userCheck) {
+  if (accountSid === "not authorized" || authToken === "not authorized") {
+    console.log("userSMS fire attempt--twilio not authorized");
+  } else {
+    client.messages
+      .create({
+        body: `${userCheck.username}, it\'s time to check in with JustNcase!`,
         from: twilioPhone,
         to: contactPhone
       })
@@ -42,7 +63,7 @@ function fireSMS(event) {
 
 // endpoint to start timer
 router.get("/start/:_id", async (req, res) => {
-  const user = await Users.findById(req.params._id, "events", (err, user) => {
+  const user = await Users.findById(req.params._id, (err, user) => {
     if (err) {
       console.log(err);
       res.end();
@@ -60,45 +81,111 @@ router.get("/start/:_id", async (req, res) => {
   const timer = setInterval(async () => {
     let userCheck = await Users.findById(
       req.params._id,
-      "events",
+
       (err, user) => {
         if (err) {
           console.log(err);
           res.end();
-        } else {
-          console.log("got user");
         }
       }
     );
 
-    count++;
-    if (count >= duration / interval || userCheck.events.checkedIn) {
-      if (userCheck.events.checkedIn) {
-        console.log("checked in");
-        Users.findByIdAndUpdate(
-          req.params._id,
-          {
-            events: { ...event, checkedIn: false }
-          },
-          { new: true },
-          (err, user) => {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("checked in success");
-            }
-          }
-        );
-      } else {
-        console.log(count);
-        console.log("Event complete");
-        clearInterval(timer);
-      }
+    // init values to be used in checkInDeadline
+    let checkinWindow = 0;
+
+    if (interval < 60 * 60 * 1000) {
+      //interval is less than an hour, will be percentage-based
+      checkinWindow = interval / 6;
+    } else if (interval < 6 * 60 * 60 * 1000) {
+      //interval still less than 6 hours, will be 15 min
+      checkinWindow = 15 * 60 * 1000;
+    } else if (interval < 24 * 60 * 60 * 1000) {
+      // interval still less than 1 day, will be 30 min
+      checkinWindow = 30 * 60 * 1000;
     } else {
-      console.log(count);
-      // passing event in to take geodata off most recent db call to build googlemaps URL for SMS fire
-      fireSMS(event);
+      //interval longer than 1 day, will be 1 hour
+      checkinWindow = 60 * 60 * 1000;
     }
+
+    count++;
+
+    // this call alerts user they need to check in (fires no matter what)
+    //todo: pass in checkinWindow to later dynamically tell the user how much time the user has to check in
+    fireUserSMS(userCheck);
+
+    // then setTimeout that fires waiting the checkinWindow amount of time, when it runs it checks the db to see if user is checked in
+    const checkInDeadline = setTimeout(async () => {
+      let userCheck = await Users.findById(
+        req.params._id,
+
+        (err, user) => {
+          if (err) {
+            console.log(err);
+            res.end();
+          }
+        }
+      );
+
+      if (count >= duration / interval) {
+        // event has ended
+        console.log(userCheck.events.checkedIn);
+        if (userCheck.events.checkedIn) {
+          // event ended and was checked in
+          console.log("\n=== event ended, final checkin on time ===\n");
+          Users.findByIdAndUpdate(
+            req.params._id,
+            {
+              events: { ...event, checkedIn: false }
+            },
+            { new: true },
+            (err, user) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("\n=== checkedIn changed back to false ===\n");
+              }
+            }
+          );
+        } else {
+          // event ended but user was NOT checked in
+
+          console.log("count: ", count, "Event ended but missed final checkin");
+          fireContactSMS(userCheck);
+        }
+        clearInterval(timer); // clear interval no matter what, event has ended
+      } else {
+        // event still going
+        console.log(userCheck.events.checkedIn);
+        if (userCheck.events.checkedIn) {
+          // event still going and was checked in
+
+          console.log(
+            "count: ",
+            count,
+            "event still going, checked in on time"
+          );
+          Users.findByIdAndUpdate(
+            req.params._id,
+            {
+              events: { ...event, checkedIn: false }
+            },
+            { new: true },
+            (err, user) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("\n=== checkedIn changed back to false ===\n");
+              }
+            }
+          );
+        } else {
+          // event still going and missed check in
+
+          console.log("count: ", count, "missed check in");
+          fireContactSMS(userCheck);
+        }
+      }
+    }, checkinWindow);
   }, interval);
 });
 
