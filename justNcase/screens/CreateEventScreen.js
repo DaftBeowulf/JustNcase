ba2c780776ae15c6dbe5c9a2eeb8050795632e88
markdavidwilliams <markwilliams.aus@gmail.com>
2019-01-04 12:50:54 -0600

    created event screen functionality

--- justNcase/screens/CreateEventScreen.js
+++ justNcase/screens/CreateEventScreen.js
@@ -1,11 +1,47 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image, ScrollView} from 'react-native';
+import { Text, View, StyleSheet, Image, ScrollView, TouchableWithoutFeedback} from 'react-native';
 import { NativeRouter, Route, Link } from "react-router-native";
 import TextInput from '../components/TextInput';
 import TextInputSpecial from '../components/TextInputSpecial';
+import axios from "axios";
 
 export default class EventScreen extends React.Component{
-render(){
+
+createEventHandler() {
+    axios.put("https://justncase.now.sh/user/event/5c2d98df11ccda4084298d7a", {
+        "username":"lambdajoe",
+        "contacts": [
+            {
+                "name": "Mark",
+                "number": "+1-512-773-1985"
+            },
+            {
+                "name": "Adam",
+                "number": "+1-512-446-2085"
+            },
+            {
+                "name": "Olu",
+                "number": "+1-512-552-3655"
+            },
+                    {
+                "name": "Chris",
+                "number": "+1-512-212-1492"
+            },
+                    {
+                "name": "Noah",
+                "number": "+1-512-101-1011"
+            }
+        ],
+        "events": {"name": "biking", "eventDuration": 15000, "checkinInterval": 5000}
+    })
+    .then(res => {
+        console.debug(res.data);
+        () => this.props.navigation.navigate("Event");
+    })
+    .catch(err => console.debug(err));
+
+}
+    render(){
     return(
         <View style={styles.overall}>
             <View style={styles.thinBox}>
@@ -34,7 +70,9 @@ render(){
                 </View>
             </View>
             <View style={styles.sidebyside}>
+            <TouchableWithoutFeedback onPress={()=> this.createEventHandler()}>
             <Image source={require('../assets/png_icons/createEventButton.png')} style={{ height:60, width:400, resizeMode:'contain', marginTop:20}} />
+            </TouchableWithoutFeedback>
             <Image source={require('../assets/png_icons/cancelbutton.png')} style={{ height:60, width:400,resizeMode:'contain', marginTop:40}} />
             </View>
         </View>
